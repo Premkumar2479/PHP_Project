b6e85c2b4d726e366ad296a1e275149d59ba2482
Premkumar2479 <pk2335301@gmail.com>
2026-01-28 01:21:10 +0530

    add new

--- Registration/db.php
+++ Registration/db.php
@@ -1,5 +1,10 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "registration_db");
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "registration_db";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
 
 if (!$conn) {
     die("Database connection failed");
